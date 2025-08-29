126dc73bbeb46c1abe462380563b74b07058640c
Sommar-skog <amandaaronsson@hotmail.com>
2025-08-29 13:52:09 +0200

    added BlogPostRepository

---
+++ src/main/java/com/example/WigellBlogAPI/repositories/BlogPostRepository.java
@@ -0,0 +1,10 @@
+package com.example.WigellBlogAPI.repositories;
+
+import com.example.WigellBlogAPI.entities.BlogPost;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BlogPostRepository extends JpaRepository<BlogPost, Long> {
+
+}
