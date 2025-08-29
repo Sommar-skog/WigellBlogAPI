1b98b7fe1b285403bf4516a87ab3c9f4bec397c3
Sommar-skog <amandaaronsson@hotmail.com>
2025-08-29 07:39:17 +0200

    Initial commit

---
+++ src/main/java/com/example/WigellBlogAPI/WigellBlogApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.WigellBlogAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WigellBlogApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WigellBlogApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/WigellBlogAPI/WigellBlogApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.WigellBlogAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WigellBlogApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
