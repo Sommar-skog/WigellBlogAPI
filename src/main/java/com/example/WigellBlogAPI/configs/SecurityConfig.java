55e89ad7a058e3202aec18a69ab36512a36e1548
Sommar-skog <amandaaronsson@hotmail.com>
2025-09-03 10:12:12 +0200

    updated SecurityConfig with converter och oauth2

--- src/main/java/com/example/WigellBlogAPI/configs/SecurityConfig.java
+++ src/main/java/com/example/WigellBlogAPI/configs/SecurityConfig.java
@@ -1,5 +1,7 @@
 package com.example.WigellBlogAPI.configs;
 
+import com.example.WigellBlogAPI.converters.JwtAuthConverter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
@@ -11,8 +13,15 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableWebSecurity
 public class SecurityConfig {
 
+    private final JwtAuthConverter jwtAuthConverter;
+
+    @Autowired
+    public SecurityConfig(JwtAuthConverter jwtAuthConverter) {
+        this.jwtAuthConverter = jwtAuthConverter;
+    }
+
     //TODO uppdatera classen för Keycloak
-    @Bean
+/*    @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests(auth -> auth
@@ -22,5 +31,26 @@ public class SecurityConfig {
                         .headers(h -> h.frameOptions(f -> f.disable())) //Så inte H2-sidan ska blockeras.
                         .formLogin(Customizer.withDefaults());
         return http.build();
+    }*/
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth ->
+                        auth
+                                .requestMatchers("/api/v2/newpost").hasRole("wigellblog-user")
+                                .requestMatchers("/api/v2/updatepost").hasAnyRole("wigellblog-user", "wigellblog-admin")
+                                .requestMatchers("/api/v2/deletepost/").hasAnyRole("wigellblog-user", "wigellblog-admin")
+                                .requestMatchers("/api/v2/count").hasRole("wigellblog-admin")
+                                .anyRequest().authenticated()
+                )
+                .oauth2ResourceServer(oauth2 ->
+                        oauth2
+                                .jwt(jwt -> jwt.jwtAuthenticationConverter(jwtAuthConverter))
+                );
+        return http.build();
     }
+
+
 }
