f46716f601efca92caef1ab6e2b01d13c09669c8
Sommar-skog <amandaaronsson@hotmail.com>
2025-09-05 08:11:58 +0200

    added BlogPostDTO

---
+++ src/main/java/com/example/WigellBlogAPI/dtos/BlogPostDTO.java
@@ -0,0 +1,66 @@
+package com.example.WigellBlogAPI.dtos;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import java.time.LocalDateTime;
+
+public class BlogPostDTO {
+
+    private Long id;
+    private String title;
+    private String content;
+    private String userId;
+    @JsonFormat(pattern = "yyyy-MM-dd", timezone = "Europe/Stockholm")
+    private LocalDateTime postedTime;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public LocalDateTime getPostedTime() {
+        return postedTime;
+    }
+
+    public void setPostedTime(LocalDateTime postedTime) {
+        this.postedTime = postedTime;
+    }
+
+    @Override
+    public String toString() {
+        return "BlogPostDTO{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", content='" + content + '\'' +
+                ", userId='" + userId + '\'' +
+                ", postedTime=" + postedTime +
+                '}';
+    }
+}
