32dcecd35dcaec5f5a01d0b9b781f4098bb77f67
Sommar-skog <amandaaronsson@hotmail.com>
2025-09-08 09:58:29 +0200

    updated data.sql with username

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,16 +1,16 @@
 
 --Kingfisher (user)
-INSERT INTO blog_post (title, content, user_id, posted_time) VALUES
-                                                    ('Post 1 of Kingfisher', 'Content 1', '7bf42d6d-5ee7-4419-bd0c-57c5928660ec', CURRENT_TIME),
-                                                    ('Post 2 of Kingfisher', 'Content 2', '7bf42d6d-5ee7-4419-bd0c-57c5928660ec', CURRENT_TIME),
-                                                    ('Post 3 of Kingfisher', 'Content 3', '7bf42d6d-5ee7-4419-bd0c-57c5928660ec', CURRENT_TIME),
-                                                    ('Post 4 of Kingfisher', 'Content 4', '7bf42d6d-5ee7-4419-bd0c-57c5928660ec', CURRENT_TIME),
-                                                    ('Post 5 of Kingfisher', 'Content 5', '7bf42d6d-5ee7-4419-bd0c-57c5928660ec', CURRENT_TIME);
+INSERT INTO blog_post (title, content, user_id, username, posted_time) VALUES
+                                                    ('Post 1 of Kingfisher', 'Content 1', '7bf42d6d-5ee7-4419-bd0c-57c5928660ec', 'death', CURRENT_TIME),
+                                                    ('Post 2 of Kingfisher', 'Content 2', '7bf42d6d-5ee7-4419-bd0c-57c5928660ec', 'death',CURRENT_TIME),
+                                                    ('Post 3 of Kingfisher', 'Content 3', '7bf42d6d-5ee7-4419-bd0c-57c5928660ec', 'death',CURRENT_TIME),
+                                                    ('Post 4 of Kingfisher', 'Content 4', '7bf42d6d-5ee7-4419-bd0c-57c5928660ec', 'death',CURRENT_TIME),
+                                                    ('Post 5 of Kingfisher', 'Content 5', '7bf42d6d-5ee7-4419-bd0c-57c5928660ec', 'death',CURRENT_TIME);
 
 --Xaden (user)
-INSERT INTO blog_post (title, content, user_id, posted_time) VALUES
-                                                    ('Post 1 of Xaden', 'Content 1', '1ab5236a-2ae4-4deb-b6f7-690a334cf93c', CURRENT_TIME),
-                                                    ('Post 2 of Xaden', 'Content 2', '1ab5236a-2ae4-4deb-b6f7-690a334cf93c', CURRENT_TIME),
-                                                    ('Post 3 of Xaden', 'Content 3', '1ab5236a-2ae4-4deb-b6f7-690a334cf93c', CURRENT_TIME),
-                                                    ('Post 4 of Xaden', 'Content 4', '1ab5236a-2ae4-4deb-b6f7-690a334cf93c', CURRENT_TIME),
-                                                    ('Post 5 of Xaden', 'Content 5', '1ab5236a-2ae4-4deb-b6f7-690a334cf93c', CURRENT_TIME);
+INSERT INTO blog_post (title, content, user_id, username, posted_time) VALUES
+                                                    ('Post 1 of Xaden', 'Content 1', '1ab5236a-2ae4-4deb-b6f7-690a334cf93c', 'shadowmarked.rider',CURRENT_TIME),
+                                                    ('Post 2 of Xaden', 'Content 2', '1ab5236a-2ae4-4deb-b6f7-690a334cf93c', 'shadowmarked.rider',CURRENT_TIME),
+                                                    ('Post 3 of Xaden', 'Content 3', '1ab5236a-2ae4-4deb-b6f7-690a334cf93c', 'shadowmarked.rider',CURRENT_TIME),
+                                                    ('Post 4 of Xaden', 'Content 4', '1ab5236a-2ae4-4deb-b6f7-690a334cf93c', 'shadowmarked.rider',CURRENT_TIME),
+                                                    ('Post 5 of Xaden', 'Content 5', '1ab5236a-2ae4-4deb-b6f7-690a334cf93c', 'shadowmarked.rider',CURRENT_TIME);
